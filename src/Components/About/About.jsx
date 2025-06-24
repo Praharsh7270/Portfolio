5da2ebb1eb268553b89ef67a62d128fef10ab75c
Praharsh <praharsh_bc22070@srmcem.ac.in>
2025-06-25 03:34:57 +0530

    changes complete

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
 import "./About.css"
+
 const About = () => {
   return (
-    <div id ="About">About</div>
+    <div id="About">
+      <div className="left">
+        <div className="circle-line">
+          <div className="circle"></div>
+          <div className="line"></div>
+          <div className="circle"></div>
+          <div className="line"></div>
+          <div className="circle"></div>
+          
+        </div>
+
+        <div className="aboutDeatls">
+            <div className="personalInfo">
+                <h1>Personal Info</h1>
+            <ul>
+                <li>
+                    <span>NAME</span> : Praharsh Singh
+                </li>
+                <li>
+                    <span>AGE</span> : 20
+                </li>
+                <li>
+                    <span>Gender</span> : MALE
+                </li>
+                <li>
+                    <span>Lang</span> : Hindi,English
+                </li>
+            </ul>
+            </div>
+
+            <div className="Education">
+                <h1>Education</h1>
+            <ul>
+                <li>
+                    <span>Course</span> : BCA
+                    <span>From</span> : SRMCEM
+                </li>
+                <li>
+                    <span>School</span> : I.C convent School
+                </li>
+            </ul>
+            </div>
+
+            <div className="Skills">
+                <h1>Skills</h1>
+            <ul>
+                <li>
+                    Mern Stack 
+                </li>
+                <li>
+                    Python & Django
+                </li>
+                <li>
+                    AI/ML
+                </li>
+                <li>
+                    Java
+                </li>
+                <li>
+                    DSA & Algorithm
+                </li>
+            </ul>
+            </div>
+        </div>
+      </div>
+
+      <div className="right">
+        {/* Add right side content here */}
+      </div>
+    </div>
   )
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import "./Home.css"
+import man from "../../assets/man.png"
+import { Typewriter } from 'react-simple-typewriter';
+
 
 const Home = () => {
-  return (
-    <div id='home'>Home</div>
-  )
+    return (
+        <div id='home'>
+
+            <div className="left">
+
+                <div className="detail">
+                    <div className="line1">I'M </div>
+                    <div className="line2">PRAHARSH SINGH</div>
+                    <div className="line3">
+                        <Typewriter
+                            words={[
+                                "WEB DEVELOPER",
+                                "FULL STACK MERN DEVELOPER",
+                                "PYTHON & DJANGO DEVELOPER",
+                                "AI/ML ENTHUSIAST",
+                                "TECH ENTHUSIAST",
+                                "PROBLEM SOLVER"
+                            ]}
+                            loop
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+                        />
+                    </div>
+                    <button>Hire me</button>
+                </div>
+            </div>
+
+            <div className="right">
+                <img src={man} alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Home
