cffc1aa09d249284417c3ed29076856a2ebe4ae0
Praharsh <praharsh_bc22070@srmcem.ac.in>
2025-06-24 14:51:04 +0530

    created

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Nav.css"
+
+const Nav = () => {
+  return (
+    <div>
+        <nav>
+            <h1>PORTFOLIO</h1>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Project</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
